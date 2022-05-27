abd5443d3b33ab84ca8ec2913f31ef74c00e38bc
Jamie Pantling <99222790+jamiepantling@users.noreply.github.com>
2022-05-27 14:52:12 -0400

    add form to template

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Widget
+from django.views.generic import CreateView, DeleteView
+from .forms import WidgetForm
+
 
 # Create your views here.
+def index(request):
+    widget_list = Widget.objects.all()
+    widget_form = WidgetForm()
+    return render(request, "index.html", {"widget_list": widget_list, "widget_form": widget_form})
+
+def add_widget(request):
+    form = WidgetForm(request.POST)
+    form.save()
+    return redirect("index")
