687e6ad83a529daf241f893684566c9d2a6e2b7b
Jamie Pantling <99222790+jamiepantling@users.noreply.github.com>
2022-05-27 15:17:47 -0400

    add quantity total

--- main_app/views.py
+++ main_app/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render, redirect
 from .models import Widget
-from django.views.generic import CreateView, DeleteView
 from .forms import WidgetForm
+from django.db.models import Sum
 
 
 # Create your views here.
 def index(request):
     widget_list = Widget.objects.all()
     widget_form = WidgetForm()
-    return render(request, "index.html", {"widget_list": widget_list, "widget_form": widget_form})
+    total = Widget.objects.aggregate(Sum("quantity"))['quantity__sum']
+
+    return render(request, "index.html", {"widget_list": widget_list, "widget_form": widget_form, "total": total})
 
 def add_widget(request):
     form = WidgetForm(request.POST)
     form.save()
+    return redirect("index")
+    
+def delete_widget(request, widget_id):
+    Widget.objects.filter(id=widget_id).delete()
     return redirect("index")
